5e86a11ef3ef1c097e06b609d5ea8fb3d59090cd
visheshbajaj <visheshbajaj1996@gmail.com>
2020-08-22 02:20:23 +0530

    adding banner

--- js/functions.js
+++ js/functions.js
@@ -4,13 +4,20 @@ function getImgDOM(src) {
 	return ele;
 }
 
-function appendImagesToDOM(srcArray, parentNode) {
-	srcArray.map(src => getImgDOM(src)).forEach(ele => parentNode.appendChild(ele))
+col1 = document.getElementById('col-1');
+col2 = document.getElementById('col-2');
+col3 = document.getElementById('col-3');
+function appendImagesToGallery(srcArray) {
+	srcArray.map(src => getImgDOM(src)).forEach((ele, i) => {
+		i % 3 === 0 && col1.appendChild(ele);
+		i % 3 === 1 && col2.appendChild(ele);
+		i % 3 === 2 && col3.appendChild(ele);
+	})
 }
 
 function loadImages() {
 	loader.style.display = 'block';
 	fetchImages()
-		.then(imgs => appendImagesToDOM(imgs, gallery))
+		.then(imgs => appendImagesToGallery(imgs))
 		.then(() => loader.style.display = 'none');
 }
